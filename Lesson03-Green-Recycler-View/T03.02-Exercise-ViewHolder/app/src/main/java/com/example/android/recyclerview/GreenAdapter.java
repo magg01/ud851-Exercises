6343db0489354f91dba551e24d0d13ef432299dc
Matthew <Magg01@gmail.com>
2021-03-04 17:42:06 +0100

    lesson 3 complete

--- Lesson03-Green-Recycler-View/T03.02-Exercise-ViewHolder/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
+++ Lesson03-Green-Recycler-View/T03.02-Exercise-ViewHolder/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
@@ -37,19 +37,19 @@ import android.widget.TextView;
  */
 public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHolder> {
 
-    // TODO (1) Create a layout resource in res/layout/ called number_list_item.xml
+    // Complete (1) Create a layout resource in res/layout/ called number_list_item.xml
 
     // Do steps 2 - 11 within number_list_item.xml
-    // TODO (2) Make the root layout a FrameLayout
-    // TODO (3) Make the width match_parent and the height wrap_content
-    // TODO (4) Set the padding to 16dp
-    // TODO (5) Add a TextView as the only child of the FrameLayout
-    // TODO (6) Give the TextView an ID "@+id/tv_item_number"
-    // TODO (7) Set the height and width to wrap_content
-    // TODO (8) Align the TextView to the start of the parent
-    // TODO (9) Center the TextView vertically in the layout
-    // TODO (10) Set the font family to monospace
-    // TODO (11) Set the text size to 42sp
+    // Complete (2) Make the root layout a FrameLayout
+    // Complete (3) Make the width match_parent and the height wrap_content
+    // Complete (4) Set the padding to 16dp
+    // Complete (5) Add a TextView as the only child of the FrameLayout
+    // Complete (6) Give the TextView an ID "@+id/tv_item_number"
+    // Complete (7) Set the height and width to wrap_content
+    // Complete (8) Align the TextView to the start of the parent
+    // Complete (9) Center the TextView vertically in the layout
+    // Complete (10) Set the font family to monospace
+    // Complete (11) Set the text size to 42sp
 
     private static final String TAG = GreenAdapter.class.getSimpleName();
 
@@ -117,15 +117,27 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
         return mNumberItems;
     }
 
-    // TODO (12) Create a class called NumberViewHolder that extends RecyclerView.ViewHolder
+    // Complete (12) Create a class called NumberViewHolder that extends RecyclerView.ViewHolder
+    public class NumberViewHolder extends RecyclerView.ViewHolder {
+        TextView listItemNumberView;
 
-    // TODO (13) Within NumberViewHolder, create a TextView variable called listItemNumberView
+        public NumberViewHolder(View itemView){
+            super(itemView);
+            listItemNumberView = (TextView) itemView.findViewById(R.id.tv_item_number);
+        }
 
-    // TODO (14) Create a constructor for NumberViewHolder that accepts a View called itemView as a parameter
-    // TODO (15) Within the constructor, call super(itemView) and then find listItemNumberView by ID
+        public void bind (int listIndex){
+            listItemNumberView.setText(String.valueOf(listIndex));
+        }
 
-    // TODO (16) Within the NumberViewHolder class, create a void method called bind that accepts an int parameter called listIndex
-    // TODO (17) Within bind, set the text of listItemNumberView to the listIndex
-    // TODO (18) Be careful to get the String representation of listIndex, as using setText with an int does something different
+    }
+    // Complete (13) Within NumberViewHolder, create a TextView variable called listItemNumberView
+
+    // Complete (14) Create a constructor for NumberViewHolder that accepts a View called itemView as a parameter
+    // Complete (15) Within the constructor, call super(itemView) and then find listItemNumberView by ID
+
+    // Complete (16) Within the NumberViewHolder class, create a void method called bind that accepts an int parameter called listIndex
+    // Complete (17) Within bind, set the text of listItemNumberView to the listIndex
+    // Complete (18) Be careful to get the String representation of listIndex, as using setText with an int does something different
 
 }
